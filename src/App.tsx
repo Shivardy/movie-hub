32eff040d2bef2245021e4bd45d6e70886c3f581
Shiva Kumar Reddy Suram <suramshivakumarreddy@gmail.com>
2021-10-05 17:22:37 -0500

    Added useAsync hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import { AppContextProvider } from './AppContext';
+import Trending from './Components/Trending';
 import useDarkMode from './hooks/useDarkMode';
 import { getTheme, GlobalStyles } from './styles/theme';
-
-const H1 = styled.h1`
-  font-size: ${(props) => props.theme.size.xxxl};
-  margin: ${(props) => props.theme.size.xxxs};
-`;
 
 function App() {
   const isDarkMode = useDarkMode();
   const theme = getTheme(isDarkMode);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <div className="app">
-        <H1>Shiva</H1>
-      </div>
+      <AppContextProvider>
+        <Trending />
+      </AppContextProvider>
     </ThemeProvider>
   );
 }
